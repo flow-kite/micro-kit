3296727d382a88666c00d9f728ee6e111b1f267f
Aladdin <zzj13865@gmail.com>
2021-01-30 09:34:18 +0800

    update

--- dist/mservice/server_webapi.go
+++ dist/mservice/server_webapi.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/http/pprof"
 	"os"
 	"strconv"
 	"strings"
@@ -109,11 +108,11 @@ func (s *WebServer) Ping(w http.ResponseWriter, req *http.Request) {
 func (s *WebServer) Serve(ctx context.T, ln net.Listener) error {
 	s.initMux()
 	s.mux.HandleFunc("/ping", s.Ping)
-	s.mux.HandleFunc("/debug/pprof/", pprof.Index)
-	s.mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	s.mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	s.mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	s.mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	// s.mux.HandleFunc("/debug/pprof/", pprof.Index)
+	// s.mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	// s.mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	// s.mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	// s.mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	for serviceName := range s.methods {
 		s.mux.HandleFunc("/api/"+serviceName+"/ping", s.Ping)
 	}
